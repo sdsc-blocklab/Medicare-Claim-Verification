b8c1c49705b4db92b6ada02d1f542be8cdb789f0
kmiyachi <kmiyachi@ucsd.edu>
2019-07-01 16:16:40 -0700

    Cleaned up code structure

--- contracts/ClaimVerification.sol
+++ contracts/ClaimVerification.sol
@@ -1,9 +1,11 @@
 pragma solidity ^0.5.0;
 
 contract ClaimVerification {
+    
     uint storedData;
 
     event ClaimCreated(uint256 id, uint256 amount, uint256 service, bytes32 patient);
+    
     uint256 claimId = 0;
 
     Patient[] patients;
@@ -47,22 +49,11 @@ contract ClaimVerification {
     }
 
 
-    function provideService(uint256 _id, bytes32 _name) public returns(uint256 serviceID) {
-        Service memory newService = Service(_id, _name);
-        return newService.id;
-    }
-    function addClaim(uint256 _amount, uint256 _service, bytes32 _patient) public returns(uint256 ClaimID) {
-        Claim memory newClaim = Claim(claimId++, _amount, serviceMap[_service], false);
-        Patient storage cPatient = patientMap[_patient];
-        cPatient.services.push(newClaim.id);
-        emit ClaimCreated(claimId, _amount, _service, _patient);
-        return newClaim.id;
-    }
-
+    // ------------------------------ Adds Users to Network --------------------------- //
     function addPatient(string memory _name) public returns(bytes32 pID) {
         bytes32 id = keccak256(abi.encodePacked(_name));
         uint256[] memory emptyList;
-        Patient memory newPatient = Patient(id, _name, emptyList);
+        Patient memory newPatient = Patient(id, _name, emptyList, []);
         patients.push(newPatient);
         return id;
     }
@@ -84,12 +75,29 @@ contract ClaimVerification {
     }
 
 
+
+    // ------------------------------ Functionality of the Network --------------------------- //
+
+    function provideService(uint256 _id, bytes32 _name) public returns(uint256 serviceID) {
+        Service memory newService = Service(_id, _name);
+        return newService.id;
+    }
+
+
+    function addClaim(uint256 _amount, uint256 _service, bytes32 _patient) public returns(uint256 ClaimID) {
+        Claim memory newClaim = Claim(claimId++, _amount, serviceMap[_service], false);
+        Patient storage cPatient = patientMap[_patient];
+        cPatient.claims.push(newClaim);
+        emit ClaimCreated(claimId, _amount, _service, _patient);
+        return newClaim.id;
+    }
+
     function verifyClaim(bytes32 _pID, uint256 _cID) public {
         //Check if claim was provided to the Patient
         ClaimVerification.Claim storage claim = claimMap[_cID];
         claim.verified = true;
-        //Pay Patient for verfiying
     }
+
 
 
 

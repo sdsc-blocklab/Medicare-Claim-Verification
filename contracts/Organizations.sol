7e2e1767f38d6b559960b5af4f6fdc102d266f47
atmartorana <atmartorana@gmail.com>
2019-07-16 21:30:49 -0700

    Documentation for Organizations.sol

--- contracts/Organizations.sol
+++ contracts/Organizations.sol
@@ -4,21 +4,20 @@ import "./SafeMath.sol";
 import "./ServiceClaim.sol";
 
 contract Organizations {
-    
-    uint storedData;
 
+    uint256 claimId; //Max value of the number of claims made
+    uint storedData; //Miscellaneous data
+
+    mapping (bytes32=>Patient) patientMap; //ID of patient to specific provider - 
+    mapping (bytes32=>Provider) providerMap; //ID of provider to specific provider -
+    mapping (bytes32=>Insurer) insurerMap; //ID of insurance provider to specific insurer
+    mapping (bytes32=>address) serviceClaimsMap; //ID of serviceClaim to the specific ServiceClaim contract instance
+
+    //Events for organization creation
     event ClaimCreated(uint256 id, uint256 amount, uint256 service, bytes32 patient);
     event PatientCreated(bytes32 id, string name);
     event ProviderCreated(bytes32 id, string name);
     event InsurerCreated(bytes32 id, string name);
-
-    uint256 claimId;
-
-    mapping (bytes32=>Patient) patientMap;
-    mapping (bytes32=>Provider) providerMap;
-    mapping (bytes32=>Insurer) insurerMap;
-
-    mapping (bytes32=>address) serviceClaimsMap;
 
     struct Patient {
         bytes32 id;
@@ -39,6 +38,12 @@ contract Organizations {
     }
 
     // ------------------------------ Adds Users to Network --------------------------- //
+
+    /** @dev add a patient to the network
+    @param _name is the name of the patient
+    @param _providerID is the current provider of the patient
+    @return pID is a bytes32 represented id number for the patient
+    */
     function addPatient(string memory _name, bytes32 _providerID) public returns(bytes32 pID) {
         bytes32 id = keccak256(abi.encodePacked(_name));
         address[] memory claimList;
@@ -50,6 +55,11 @@ contract Organizations {
         return id;
     }
 
+    /** @dev add a provider to the network
+    @param _name name of the provider
+    @param _insurerID id of insurance provider
+    @return the pID of the health provider -
+     */
     function addProvider(string memory _name, bytes32 _insurerID) public returns(bytes32 pID) {
         bytes32 id = keccak256(abi.encodePacked(_name));
         bytes32[] memory emptyList;
@@ -61,6 +71,10 @@ contract Organizations {
         return id;
     }
 
+    /** @dev add an insurnace provider to the network
+    @param _name the name of the insurance provider
+    @return the pID of the insurance provider
+    */
     function addInsurer(string memory _name) public returns(bytes32 pID) {
         bytes32 id = keccak256(abi.encodePacked(_name));
         bytes32[] memory emptyList;
@@ -73,14 +87,12 @@ contract Organizations {
 
     // ------------------------------ Functionality of the Network --------------------------- //
 
-    function addClaim(bytes32 _serviceClaimID, uint256 _amount, uint256 _service, bytes32 _patient) public returns(uint256 ClaimID) {
-        Patient storage cPatient = patientMap[_patient];
-        ServiceClaim myServiceClaim = ServiceClaim(serviceClaimsMap[_serviceClaimID]);
-        uint256 newClaimID = myServiceClaim.addClaim(claimId++,_amount);
-        emit ClaimCreated(newClaimID, _amount, _service, _patient);
-        return newClaimID;
-    }
-
+    /** @dev create an instance of a ServiceClaim contract
+    @param _name name of the service
+    @param _providerID provider ID
+    @param _patientID patient ID
+    @return serviceContractHash is the id of the ServiceClaim contract
+    */
     function newServiceClaim(string memory _name, bytes32 _providerID, bytes32 _patientID) public returns(bytes32 serviceContractHash) {
         uint256 id = uint(keccak256(abi.encodePacked(_name)));
         ServiceClaim serviceClaim = new ServiceClaim(_providerID, _patientID, id, _name);
@@ -91,15 +103,36 @@ contract Organizations {
         return serviceClaimID;
     }
 
-    function payProvider(ServiceClaim _serviceClaim) internal {
-        //require(_serviceClaim.claim().verified == true, "User has not verified service");
-        _serviceClaim.payProvider();
+    /** @dev add a claim to a ServiceClaim contract i.e. the provider wants to file a claim for a service rendered
+    @param _serviceClaimID the ID of the ServiceClaim contract
+    @param _amount the amount to be claimed by the provider
+    @param _service the service code provided
+    @param _patient the patient id
+    @return ClaimID the ID of the claim
+    */
+    function addClaim(bytes32 _serviceClaimID, uint256 _amount, uint256 _service, bytes32 _patient) public returns(uint256 ClaimID) {
+        Patient storage cPatient = patientMap[_patient];
+        ServiceClaim myServiceClaim = ServiceClaim(serviceClaimsMap[_serviceClaimID]);
+        uint256 newClaimID = myServiceClaim.addClaim(claimId++,_amount);
+        emit ClaimCreated(newClaimID, _amount, _service, _patient);
+        return newClaimID;
     }
 
+    /** @dev invoke the verifyClaim function in the ServiceClaim contract
+    @param _serviceClaimID the id of the ServiceClaim contract
+    */
     function verifyClaim(bytes32 _serviceClaimID) public {
         ServiceClaim myServiceClaim = ServiceClaim(serviceClaimsMap[_serviceClaimID]);
         myServiceClaim.verifyClaim();
         payProvider(myServiceClaim);
+    }
+
+    /** @dev invoke the payProvider function in the ServiceClaim contract, may only be called from within Organizations.sol
+    @param _serviceClaim an instance of the ServiceClaim contract
+    */
+    function payProvider(ServiceClaim _serviceClaim) internal {
+        //require(_serviceClaim.claim().verified == true, "User has not verified service");
+        _serviceClaim.payProvider();
     }
     
 }

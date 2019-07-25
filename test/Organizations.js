9cb4e5933f961a252b6c2e9c29b2668dc34cc49d
kmiyachi <kmiyachi@ucsd.edu>
2019-07-25 15:27:27 -0700

    First Organization Test

--- test/Organizations.js
+++ test/Organizations.js
@@ -1,15 +1,15 @@
 const Organizations = artifacts.require("Organizations");
 
 contract('Organizations', (accounts) => {
-	console.log(accounts);
+	//console.log(accounts);
 	beforeEach(async function(){
 		this.organizations = await Organizations.new();
 	});
 
-	it('should work', async () => {
+	it('Organization Contract is properly deployed', async () => {
 		const organizationsInstance = await Organizations.deployed();
-		console.log(organizationsInstance);
-		assert.equal(balance.valueOf(), 10000, "10000 wasn't in the first account");
+		const oAddress = organizationsInstance.address 
+		assert(oAddress, "Organization address does not exist");
 	});
 });
 

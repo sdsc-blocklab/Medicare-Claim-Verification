65eb0499f4bbaee3045de621b81536f2e78ce16c
kmiyachi <kmiyachi@ucsd.edu>
2019-07-26 14:46:50 -0700

    cleaned up testing a little

--- test/Organizations.js
+++ test/Organizations.js
@@ -2,35 +2,6 @@ const Organizations = artifacts.require("Organizations");
 const ServiceClaim = artifacts.require("ServiceClaim");
 
 
-function bytesToString(bytes) {
-    if (bytes.length && !bytes.map) bytes = [bytes]
-    let arr = bytes.map(b => Array.from(ethers.utils.arrayify(b)))
-    let utf8 = [].concat(...arr).filter(x => x)
-    return ethers.utils.toUtf8String(utf8)
-  }
-
-function numberToBytes(number) {
-    let bytes = ethers.utils.hexlify(number)
-    return '0x' + ('0'.repeat(64) + bytes.substr(2)).substr(-64)
-}
-
-function stringToBytes(text, len = 0) {
-    text = text || ''
-    let data = ethers.utils.toUtf8Bytes(text)
-    let padding = 64 - ((data.length * 2) % 64)
-    data = ethers.utils.hexlify(data)
-    data = data + '0'.repeat(padding)
-    if (len <= 0) return data
-
-    data = data.substring(2)
-    data = data.match(/.{1,64}/g)
-    data = data.map(v => '0x' + v)
-    while (data.length < len) {
-      data.push('0x00')
-    }
-    return data
-}
-
 let organizationalInstance;
 let insurer; 
 let insurerID; 

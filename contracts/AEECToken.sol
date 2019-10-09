c9787e399948f7f352259e2c50a6e99ac3242c13
atmartorana <atmartorana@gmail.com>
2019-10-08 17:42:33 -0700

    Added boilerplate ERC20

---
+++ contracts/AEECToken.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.5.0;
+
+import "./SafeMath.sol";
+
+contract AEECToken is ERC20, SafeMath {
+	string public name = "AEECToken";
+	string public symbol = "AEEC";
+	
+	address public 
+	uint public insurer = 1000000;
+
+	constructor() public {
+
+	}
+}

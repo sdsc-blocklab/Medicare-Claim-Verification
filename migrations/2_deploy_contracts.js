62a90bafa9d923c02f766d8ad04836dac066c056
Danny Fung <danny@cryptos-MBP.lan1>
2020-01-18 17:31:36 -0800

    Beginning to deploy all contracts through migration for usage in App.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,12 +3,18 @@ var Organizations = artifacts.require("./Organizations.sol");
 var ServiceClaim = artifacts.require("./ServiceClaim.sol");
 var AEECToken = artifacts.require("./AEECToken.sol");
 var Insurer = artifacts.require("./Insurer.sol");
+var Provider = artifacts.require("./Provider.sol");
+var Patient = artifacts.require("./Patient.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SafeMath);
   deployer.deploy(AEECToken).then(function(){
     deployer.deploy(Organizations, AEECToken.address);
-    return deployer.deploy(Insurer, AEECToken.address, "CMS");
+    deployer.deploy(Insurer, AEECToken.address, "CMS").then(function () {
+      deployer.deploy(Provider, keccak256(abi.encodePacked("UCSD Medical")), "UCSD Medical").then(function(){
+        return deployer.deploy(Patient, keccak256(abi.encodePacked("Ken")), "Ken");
+      });
+    });
   });
   //deployer.deploy(ServiceClaim);
 };
